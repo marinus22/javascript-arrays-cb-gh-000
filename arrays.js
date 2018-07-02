f639afdabe80ee426bf0373638952643d4ce43f9
Sara McCarthy <sc.mccarthy1123@gmail.com>
2018-07-02 11:54:13 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,9 @@
+var chocolateBars = ['chocolateBars',
+                      'snickers',
+                      'hundred grand',
+                      'kitkat',
+                      'skittles'];
+
+function addElementToBeginningOfArray(array, element) {
+  
+}
