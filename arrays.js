a63db126e190d42c8e89418bf1dfa0621111c08b
Sara McCarthy <sc.mccarthy1123@gmail.com>
2018-07-02 12:08:32 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,5 +5,43 @@ var chocolateBars = ['chocolateBars',
                       'skittles'];
 
 function addElementToBeginningOfArray(array, element) {
-  
+  return [element, ...array];
+}
+
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array = [element, ...array];
+  return array;
+}
+
+function addElementToEndOfArray(array, element) {
+  return [...array, element];
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array = [...array, element];
+  return array;
+}
+
+function accessElementInArray(array, index) {
+  return array[index];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift();
+  return array;
+}
+
+function removeElementFromBeginningOfArray(aray) {
+  array.slice(1);
+  return array;
+}
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop();
+  return array;
+}
+
+function removeElementFromEndOfArray(array) {
+  return array.slice(0, array.length - 1);
 }
